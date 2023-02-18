242d2753800412130288c8c30f793a1ad478891b
Maciej Glownia <maciej.glownia@infor.com>
2023-02-18 16:22:02 +0100

    Initial commit

---
+++ src/main/java/pl/glownia/maciej/springbootdailywins/SpringBootDailyWinsApplication.java
@@ -0,0 +1,13 @@
+package pl.glownia.maciej.springbootdailywins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDailyWinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDailyWinsApplication.class, args);
+	}
+
+}
