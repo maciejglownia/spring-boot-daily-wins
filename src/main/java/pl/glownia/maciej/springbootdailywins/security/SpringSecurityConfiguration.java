48ab39aafd555c74b4190e3aa30cc384d48f00d9
Maciej Glownia <maciej.glownia@infor.com>
2023-02-22 08:53:30 +0100

    Configure Spring Security to get H2 console working

--- src/main/java/pl/glownia/maciej/springbootdailywins/security/SpringSecurityConfiguration.java
+++ src/main/java/pl/glownia/maciej/springbootdailywins/security/SpringSecurityConfiguration.java
@@ -2,13 +2,17 @@ package pl.glownia.maciej.springbootdailywins.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.function.Function;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 class SpringSecurityConfiguration {
@@ -38,5 +42,18 @@ class SpringSecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated());
+        http.formLogin(withDefaults());
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+
+        return http.build();
     }
 }
